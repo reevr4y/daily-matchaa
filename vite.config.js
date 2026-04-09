0fe3aad651febb2408dfa456d473683fc3ee2e70
reevr4y <henq372@gmail.com>
2026-04-09 12:48:05 +0700

    Cleanup: Remove unused files and directories

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/daily-life-tracker/',
+  base: '/daily-matchaa/',
   
   esbuild: {
     drop: ['console', 'debugger']
